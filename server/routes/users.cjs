635f9b920676944389e6cc36f8b339ea7e68bd13
penasomariatheresa26-cyber <penasomariatheresa26@gmail.com>
2026-05-28 03:56:16 +0800

    Update users.cjs

--- server/routes/users.cjs
+++ server/routes/users.cjs
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const db = require('../db.cjs'); // Adjust this path if your db configuration file is elsewhere
 
 // ============================================
